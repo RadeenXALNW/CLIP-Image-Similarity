6b4e83b73c3f0fb40844620a616888cffe202a89
Radeen Mostafa <66905164+RadeenXALNW@users.noreply.github.com>
2024-04-06 16:16:45 +0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,3 @@
+# TODO: Multiple errors can be wrapped inside an exception.
+#       WfcApiException should do recursive error checking to locate
+#       and store an arbitrary number of nested errors.
